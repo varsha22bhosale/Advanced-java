1cf197473d6e6e01296b8f88d2d4a3b1a2e5cb99
Varsha Bhosale <88528164+varsha22bhosale@users.noreply.github.com>
2021-10-25 23:58:37 +0530

    Frame program

---
+++ AWT Controls/Myframe1.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+class Myframe1 extends Frame
+{
+	Myframe1(String title)
+	  {
+	  super(title);
+	  }
+    public static void main (String args[])
+  {
+
+	  Myframe1 M1=new Myframe1("jungkook");
+	  M1.setVisible(true);
+	  M1.setSize(700,700);
+	  
+  }
+
+}
